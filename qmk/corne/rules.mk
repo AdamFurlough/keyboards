a4d9973fd3b95dc704a6b41b4f839630e0af0945
AdamFurlough <111088672+AdamFurlough@users.noreply.github.com>
2023-12-02 20:50:41 -0500

    Delete qmk/corne/rules.mk

--- qmk/corne/rules.mk
+++
@@ -1,6 +0,0 @@
-MOUSEKEY_ENABLE = yes      # enables mouse keys
-COMBO_ENABLE = yes         # enables combos
-KEY_OVERRIDE_ENABLE = yes  # enables use of shift overrides
-LTO_ENABLE      = yes      # enables link time optimization to reduce binary size
-RGBLIGHT_ENABLE = no       # disables WS2812 RGB underlight
-OLED_ENABLE     = no       # disables oled screen
